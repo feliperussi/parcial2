36dc96f2ad2b174103326ba10b65a969da81ec1a
feliperussi <af.ariasr@uniandes.edu.co>
2022-12-02 04:09:48 -0500

    parcial 2

--- src/index.js
+++ src/index.js
@@ -4,18 +4,33 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import localeEsMessages from "./locales/es";
+import localeEnMessages from "./locales/en";
+import {IntlProvider} from "react-intl";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+const language = navigator.language.split(/[-_]/)[0];
+if (language === 'es') {
+      root.render(
+      <IntlProvider locale={language} messages={localeEsMessages}>
+            <App />
+      </IntlProvider>
+      );
+}
+else {
+      root.render(
+      <IntlProvider locale={language} messages={localeEnMessages}>
+            <App />
+      </IntlProvider>
+      );
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister();
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
